09ac9e00193bda15cc367e93b60379ce890187f2
John Ric Merque <johnricmer@gmail.com>
2022-12-10 18:37:01 +0800

    tried using list as rudimentary stack

--- queues.py
+++ queues.py
@@ -36,6 +36,17 @@ class Stack(Queue):
 
 # Testing LIFO Stack
 
-lifo = Stack("1st", "2nd", "3rd")
-for element in lifo:
-    print(element)
+# lifo = Stack("1st", "2nd", "3rd")
+# for element in lifo:
+#     print(element)
+
+# Using Lists as rudimentary stack
+
+lifo = []
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(lifo.pop())
+print(lifo.pop())
+print(lifo.pop())
