a05c4a62a7d3cf256292b8503186793ba488be6b
John Ric Merque <johnricmer@gmail.com>
2022-12-10 18:31:35 +0800

    tested Stack class

--- queues.py
+++ queues.py
@@ -34,3 +34,8 @@ class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
 
+# Testing LIFO Stack
+
+lifo = Stack("1st", "2nd", "3rd")
+for element in lifo:
+    print(element)
