dd3cdf929197cdb6a3af8d3c3af0224a0f66002f
John Ric Merque <johnricmer@gmail.com>
2022-12-10 17:57:40 +0800

    tested FIFO queue in interactive mode

--- queues.py
+++ queues.py
@@ -11,4 +11,10 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
-        
+
+# Testing FIFO queue
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
