3c1ea9691b877a1c81d4df18253f1db14e664326
John Ric Merque <johnricmer@gmail.com>
2022-12-13 07:13:47 +0800

    initialized class constructor for City and inherited named tuple module

--- graph.py
+++ graph.py
@@ -1,6 +1,15 @@
 import networkx as nx
 import os
 os.add_dll_directory("C:/Program Files/Graphviz/bin")
+from typing import NamedTuple
 
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 graph.nodes["london"]
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+    
