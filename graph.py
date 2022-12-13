d9231198e261d5822205a8994c60a05890217715
John Ric Merque <johnricmer@gmail.com>
2022-12-13 08:53:57 +0800

    added function to sort cities by latitude

--- graph.py
+++ graph.py
@@ -41,6 +41,10 @@ def by_distance(weights):
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
 
 # TESTS
 
